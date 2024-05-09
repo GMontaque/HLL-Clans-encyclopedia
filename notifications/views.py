0732e07f3de70c1c19f622ca9b932d12d5b4de2b
Giulio Montaque <giuliom@hotmail.co.uk>
2024-05-09 07:31:33 +0000

    create views html page and url link

--- notifications/views.py
+++ notifications/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse
+from django.views import generic
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+
 
 # Create your views here.
+def notifications(request):
+    return render(request, 'notifications.html')
