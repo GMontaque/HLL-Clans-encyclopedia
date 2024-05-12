825a4d070084d15fd3a5ad880fec7930d7aa755b
Giulio Montaque <giuliom@hotmail.co.uk>
2024-05-12 11:27:48 +0000

    update notifications model and add to admin page

--- notifications/models.py
+++ notifications/models.py
@@ -3,17 +3,15 @@ from django.contrib.auth.models import User
 from clan_pages.models import Clan
 from matches.models import Match
 
-# Create your models here.
-status = {
-    "completed": 0,
-    "in-progress": 1,
-    "rejected ": 2,
-}
-
 class Notification(models.Model):
-    content = models.CharField((""), max_length=50)
-    issuer = models.ForeignKey(User, verbose_name=(""), on_delete=models.CASCADE,related_name='issuer')
-    receiver = models.ForeignKey(User, verbose_name=(""), on_delete=models.CASCADE,related_name='receiver')
-    status = models.IntegerField((""))
-    clan = models.ForeignKey(Clan, verbose_name=(""), on_delete=models.CASCADE)
-    match = models.ForeignKey(Match, verbose_name=(""), on_delete=models.CASCADE)
+    content = models.CharField(("Content"), max_length=50)
+    issuer = models.ForeignKey(User, verbose_name=("Issuer"), on_delete=models.CASCADE,related_name='issuer')
+    receiver = models.ForeignKey(User, verbose_name=("Receiver"), on_delete=models.CASCADE,related_name='receiver')
+    # status = models.IntegerField((""))
+    status = models.CharField(max_length=50, choices=[
+        ("completed", "completed"),
+        ("in-progress", "in-progress"),
+        ("rejected", "rejected"),
+    ])
+    clan = models.ForeignKey(Clan, verbose_name=("Clan"), on_delete=models.CASCADE)
+    match = models.ForeignKey(Match, verbose_name=("Match"), on_delete=models.CASCADE)
