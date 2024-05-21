223f66e5bc7204ea58e4a70bdd4185fff0b6dfc4
Giulio Montaque <giuliom@hotmail.co.uk>
2024-05-21 23:20:09 +0000

    allow clan_rep and admin to edit clan page

--- clan_pages/views.py
+++ clan_pages/views.py
@@ -1,11 +1,7 @@
-from django.shortcuts import render, get_object_or_404, reverse
-from django.views import generic
-from django.http import HttpResponse
-from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
 from .forms import CreateClan
 from .models import Clan
-from django.contrib import messages
-
 
 # Create your views here.
 # clan indiviual page view
@@ -32,4 +28,14 @@ def clan_creation(request):
 
 
 def edit_clan_page(request, clan_name):
-    pass
+    clan = get_object_or_404(Clan, clan_name=clan_name)
+    
+    if request.method == 'POST':
+        form = CreateClan(request.POST, instance=clan)
+        if form.is_valid():
+            form.save()
+            return redirect('clan_page', clan_name=clan.clan_name)
+    else:
+        form = CreateClan(instance=clan)
+    
+    return render(request, 'edit_clan_page.html', {'edit_clan_form': form, 'clans': clan})
