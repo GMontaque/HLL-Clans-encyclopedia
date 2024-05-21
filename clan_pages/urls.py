2e9c6d3cf677b255079f5e523ccdc111ba98dd14
Giulio Montaque <giuliom@hotmail.co.uk>
2024-05-21 07:22:09 +0000

    move url path for clan page in clan page app

--- clan_pages/urls.py
+++ clan_pages/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from. import views
 
 urlpatterns = [
-    path('', views.clan_page, name='clan_page'),
-    path('creation/', views.clan_creation, name='clan_creation'),
+    # clan pages are displayed through the index app url
+    path('clan/<str:clan_name>/', views.clan_page, name='clan_page'),
+    path('clan_creation/', views.clan_creation, name='clan_creation'),
+    path('clan/edit/<str:clan_name>/', views.edit_clan_page, name='edit_clan_page'),
 ]
--- index/urls.py
+++ index/urls.py
@@ -5,5 +5,5 @@ from clan_pages.views import clan_page
 urlpatterns = [
     path('', views.index, name='index'),
     # displays the individual clan page
-    path('clan/<str:clan_name>/', clan_page, name='clan_page'),
+    
 ]
