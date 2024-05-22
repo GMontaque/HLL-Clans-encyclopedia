4659da95bfb6875499c05de60942559efe076324
Giulio Montaque <giuliom@hotmail.co.uk>
2024-05-22 08:14:48 +0000

    delete duplicate code

--- clan_pages/admin.py
+++ clan_pages/admin.py
@@ -2,9 +2,6 @@ from django.contrib import admin
 from .models import Clan
 
 # Register your models here.
-from django.contrib import admin
-from .models import Clan
-
 class ClanAdmin(admin.ModelAdmin):
     list_display = ('clan_name', 'user')
 
