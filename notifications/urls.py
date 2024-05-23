314c29148a1d572aeca6b8e6a0259e30d8fc2788
Giulio Montaque <giuliom@hotmail.co.uk>
2024-05-23 07:28:07 +0000

    allow admin to update notification status

--- notifications/urls.py
+++ notifications/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.notifications, name='notifications'),
     path('admin_ticket/', views.admin_notifications, name='admin_ticket'),
     path('notification/<int:notification_id>/', views.show_notification, name='individual_notification'),
+    path('notification/<int:pk>/update/<str:status>/', views.update_notification_status, name='update_notification_status')
 ]
--- notifications/views.py
+++ notifications/views.py
@@ -15,6 +15,15 @@ def notifications(request):
         temp = Notification.objects.none() 
     return render(request, 'notifications.html', {'notifications':temp})
 
+def update_notification_status(request, pk, status):
+    notification = get_object_or_404(Notification, pk=pk)
+    if status in ['completed', 'rejected']:
+        notification.status = status
+        notification.save()
+    if request.user.is_authenticated:
+        temp = Notification.objects.filter(receiver=request.user)
+    return render(request, 'notifications.html', {'notifications':temp})
+
 def admin_notifications(request):
     if request.method == "POST":
         print("Received a POST request")
@@ -25,10 +34,14 @@ def admin_notifications(request):
             messages.add_message(
                 request, messages.SUCCESS,
                 'notification sent'
-            )  
+            )
+            if request.user.is_authenticated:
+                temp = Notification.objects.filter(receiver=request.user)
+            else:
+                temp = Notification.objects.none() 
+            return render(request, 'notifications.html', {'notifications':temp})  
     form = CreateNotification()
     return render(request, 'admin_ticket.html',{'createNotification': form})
-
 
 def show_notification(request, notification_id):
     indiv_notification = get_object_or_404(Notification, pk=notification_id)
