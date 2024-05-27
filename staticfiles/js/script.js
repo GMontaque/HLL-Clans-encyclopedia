21cdd3c3deb0583e04e0ca970600de63671d84ba
Giulio Montaque <giuliom@hotmail.co.uk>
2024-05-27 12:05:45 +0000

    update staticfiles folder with css and img content

---
+++ staticfiles/js/script.js
@@ -0,0 +1,17 @@
+// displays an icone and text based on value of notification message status
+
+let notification_status = document.getElementsByClassName("status-icon");
+
+for (let i = 0; i < notification_status.length; i++) {
+	statusVal = notification_status[i].textContent.trim();
+	statusVal = statusVal.charAt(0).toUpperCase() + statusVal.slice(1);
+	let icon = "";
+	if (statusVal == "Completed") {
+		icon = '<i class="fa-solid fa-circle-check fa-2xl"></i>';
+	} else if (statusVal == "Rejected") {
+		icon = '<i class="fa-solid fa-circle-xmark fa-2xl"></i>';
+	} else {
+		icon = '<i class="fa-solid fa-clock fa-2xl"></i>';
+	}
+	notification_status[i].innerHTML = icon + " " + statusVal;
+}
