b9fea7cd63b52c1f5f4684a9cef5b27b3f0e51cb
Giulio Montaque <giuliom@hotmail.co.uk>
2024-05-27 11:49:12 +0000

    update base html navbar and add in pop up message

--- index/views.py
+++ index/views.py
@@ -4,8 +4,23 @@ from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from clan_pages.models import Clan
 from profiles.models import UserProfile
+from django.contrib.auth import login, logout
+from django.contrib import messages
+from django.shortcuts import redirect
 
+# displays index page with clans
 def index(request):
-    clans = Clan.objects.all()
+    clans = Clan.objects.order_by('clan_name')
     return render(request, 'index.html', {'clans': clans})
-  
+
+# displays logged in message
+def login_popup(request, *args, **kwargs):
+    response = login(request, *args, **kwargs)
+    messages.success(request, 'Signed in successfully')
+    return redirect('index.html')
+
+# displays logged out message
+def logout_popup(request, *args, **kwargs):
+    response = logout(request, *args, **kwargs)
+    messages.info(request, 'Signed out successfully')
+    return redirect('index.html') 
