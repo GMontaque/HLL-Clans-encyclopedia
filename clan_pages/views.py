9272bde2a8f05018bd0a80f165427c10647dcdf0
Giulio Montaque <giuliom@hotmail.co.uk>
2024-06-02 11:43:35 +0000

    update clan creation so admin can create a clan for an user

--- clan_pages/views.py
+++ clan_pages/views.py
@@ -4,30 +4,45 @@ from .forms import CreateClan
 from .models import Clan
 from matches.models import Match
 from django.db.models import Q
+from index.views import error_view
+from django.contrib.auth.models import User
 
 # displays indiviual clan page
 def clan_page(request, clan_name):
-    clan = Clan.objects.get(clan_name=clan_name)
-    matches = Match.objects.filter((Q(inviter_clan_id=clan.id) | Q(invitee_clan_id=clan.id)) & Q(is_accepted="accepted"))
+    try:
+        clan = Clan.objects.get(clan_name=clan_name)
+        matches = Match.objects.filter((Q(inviter_clan_id=clan.id) | Q(invitee_clan_id=clan.id)) & Q(is_accepted="accepted"))
+    except:
+        return error_view(request)
     return render(request, 'clan_page.html', {'clan': clan, 'current_user': request.user, 'matches':matches})
 
 # creates Clan page
 def clan_creation(request):
+    users = None
+    if request.user.username == "admin":
+        users = User.objects.all()
     # checks for a post value from the method on the form
     if request.method == "POST":
         print("Received a POST request")
-        
+        # form data to be passed to database
+        data = {'clan_name': request.POST.get('clan_name'),
+        'content': request.POST.get('content'),
+        'image_url': request.POST.get('image_url'),
+        'discord_url': request.POST.get('discord_url'),
+        'website_url': request.POST.get('website_url')}
+        # checks if user is admin or clan rep
+        user = User.objects.get(username=request.POST.get('selected_user')) if request.POST.get('selected_user') else request.user
         # Check if the user already has a clan
-        if Clan.objects.filter(user=request.user).exists():
+        if Clan.objects.filter(user=user).exists():
             messages.add_message(request, messages.ERROR, "ERROR, a clan is already associated with this account")
             return redirect('index')
 
-        form = CreateClan(data=request.POST)
+        form = CreateClan(data=data)
         # saves the newly created form and redirects to home page(index)
         if form.is_valid():
             print("Form is valid, creating clan")
             clan = form.save(commit=False)
-            clan.user = request.user
+            clan.user = user
             clan.save()
             messages.add_message(request, messages.SUCCESS, 'Clan created successfully')
             return redirect('index')
@@ -36,7 +51,7 @@ def clan_creation(request):
             
     form = CreateClan()
     # displays clan creation page and form
-    return render(request, 'clan_creation.html', {'clanCreation': form})
+    return render(request, 'clan_creation.html', {'clanCreation': form, 'users':users})
 
 # edits clan page
 def edit_clan_page(request, clan_name):
