dd63c3ee69dea6c21369f59a0ef7f0afb78d74cb
Giulio Montaque <giuliom@hotmail.co.uk>
2024-06-15 07:08:05 +0000

    update label name on sign up form

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -15,3 +15,7 @@ for (let i = 0; i < notification_status.length; i++) {
 	}
 	notification_status[i].innerHTML = icon + " " + statusVal;
 }
+
+// updates label text of registration form
+const emailLabel = document.querySelector('#id_email').previousElementSibling;
+emailLabel.innerHTML = 'Email:';
