416c672bc7766a4155d433622fd6bc45f952d4a2
Giulio Montaque <giuliom@hotmail.co.uk>
2024-06-15 07:06:57 +0000

    update view functions to class functions

--- index/urls.py
+++ index/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 from clan_pages.views import clan_page
+from .views import IndexView, LoginPopupView, LogoutPopupView
 
 urlpatterns = [
     # url path to index page
-    path('', views.index, name='index'),
+    path('', IndexView.as_view(), name='index'),
+    path('login/', LoginPopupView.as_view(), name='login_popup'),
+    path('logout/', LogoutPopupView.as_view(), name='logout_popup'),
 ]
--- index/views.py
+++ index/views.py
@@ -1,33 +1,38 @@
-from django.shortcuts import render, get_object_or_404, reverse
+from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.views import generic
-from django.http import HttpResponse
-from django.http import HttpResponseRedirect
-from clan_pages.models import Clan
-from profiles.models import UserProfile
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import login, logout
 from django.contrib import messages
-from django.shortcuts import redirect
+from django.views.generic.base import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from clan_pages.models import Clan
+from profiles.models import UserProfile
 
 
 # displays index page with clans
-def index(request):
-    clans = Clan.objects.order_by('clan_name')
-    return render(request, 'index.html', {'clans': clans})
+class IndexView(generic.ListView):
+    template_name = 'index.html'
+    context_object_name = 'clans'
+
+    def get_queryset(self):
+        return Clan.objects.order_by('clan_name')
 
 
-# displays logged in message
-def login_popup(request, *args, **kwargs):
-    response = login(request, *args, **kwargs)
-    messages.success(request, 'Signed in successfully')
-    return redirect('index.html')
+class LoginPopupView(LoginRequiredMixin, TemplateView):
+    template_name = 'index.html'
+    
+    def get(self, request, *args, **kwargs):
+        response = login(request, *args, **kwargs)
+        messages.success(request, 'Signed in successfully')
+        return redirect('index')
 
-
-# displays logged out message
-def logout_popup(request, *args, **kwargs):
-    response = logout(request, *args, **kwargs)
-    messages.info(request, 'Signed out successfully')
-    return redirect('index.html')
-
+class LogoutPopupView(LoginRequiredMixin, TemplateView):
+    template_name = 'index.html'
+    
+    def get(self, request, *args, **kwargs):
+        response = logout(request, *args, **kwargs)
+        messages.info(request, 'Signed out successfully')
+        return redirect('index')
 
 def error_view(request):
     return render(request, '404.html')
