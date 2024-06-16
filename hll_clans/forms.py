8c822a9fd44133c573ee89688b07c836cfec4844
Giulio Montaque <giuliom@hotmail.co.uk>
2024-06-16 09:40:10 +0000

    update form as per python validator

--- hll_clans/forms.py
+++ hll_clans/forms.py
@@ -2,7 +2,6 @@ from allauth.account.forms import SignupForm
 from django import forms
 from profiles.models import UserProfile
 
-
 class CustomSignupForm(SignupForm):
     clan_rep = forms.BooleanField(label="Clan Rep", required=False)
     memorable_name = forms.CharField(label="Memorable Name", max_length=100)
@@ -10,11 +9,7 @@ class CustomSignupForm(SignupForm):
     def save(self, request):
         print(request)
         user = super(CustomSignupForm, self).save(request)
-        userprofile = UserProfile(
-            user=user,
-            memorable_name=self.cleaned_data['memorable_name'],
-            clan_rep=self.cleaned_data['clan_rep']
-        )
+        userprofile = UserProfile(user=user, memorable_name=self.cleaned_data['memorable_name'], clan_rep = self.cleaned_data['clan_rep']  )
         userprofile.save()
 
         return user
