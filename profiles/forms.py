6b067b379de9f622849ff51df60eb1ec430c6c7b
Giulio Montaque <giuliom@hotmail.co.uk>
2024-06-17 16:35:45 +0000

    update form to pass python validator

--- profiles/forms.py
+++ profiles/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from .models import UserProfile
 
+
 class PasswordResetRequestForm(forms.Form):
     email = forms.EmailField(label="Email", max_length=254)
     verification_value = forms.CharField(
@@ -17,9 +18,11 @@ class PasswordResetRequestForm(forms.Form):
                 user = User.objects.get(email=email)
                 user_profile = UserProfile.objects.get(user=user)
                 if value != user_profile.memorable_name:
-                    raise forms.ValidationError("Verification value is incorrect.")
+                    raise forms.ValidationError("Verification value"
+                                                "is incorrect.")
             except (User.DoesNotExist, UserProfile.DoesNotExist):
-                raise forms.ValidationError("No account found with this email address.")
+                raise forms.ValidationError("No account found with this"
+                                            "email address.")
         return value
 
     def clean(self):
@@ -30,6 +33,7 @@ class PasswordResetRequestForm(forms.Form):
                 "No account found with this email address."
             )
         return cleaned_data
+
 
 class PasswordChangeForm(forms.Form):
     username = forms.CharField(
