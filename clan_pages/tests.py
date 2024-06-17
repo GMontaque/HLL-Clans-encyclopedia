c2720fe61145192ffe9cdf5bdf58adf5ec5d0225
Giulio Montaque <giuliom@hotmail.co.uk>
2024-06-17 09:56:47 +0000

    create unitests for different sections of the python code

--- clan_pages/tests.py
+++ clan_pages/tests.py
@@ -1,3 +1,71 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import Clan
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your tests here.
+class clanTestCase(TestCase):
+
+    def setUp(self):
+        user = User.objects.create(username="test1", email="test@test.com")
+        user.set_password("test123")
+        user.save()
+
+        clan = Clan.objects.create(
+            user=user,
+            clan_name="test-clan",
+            content="ffsdfgsfsdfsdfds",
+            discord_url="afafagag"
+        )
+        clan.save()
+        self.client = Client()
+    
+    def test_model_creation(self):
+        # test for model
+        clan = Clan.objects.get(clan_name="test-clan")
+        self.assertEqual(clan.clan_name, "test-clan")
+    
+    def test_clan_creation_view(self):
+        # clan creation view
+        self.client.login(username="test1", password="test123")
+        data = {
+            "clan_name": "test-clan",
+            "content": "ffsdfgsfsdfsdfds",
+            "discord_url": "afafagag"
+        }
+        response = self.client.post(reverse("clan_creation"), data)
+        self.assertEqual(response.status_code, 302)
+
+    def test_clan_page_view(self):
+        # clan page view
+        self.client.login(username="test1", password="test123")
+        clan_name = "test-clan"
+        url = reverse("clan_page", kwargs={"clan_name": clan_name})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_edit_clan_page_view_get(self):
+        # edit clan page - GET 
+        self.client.login(username="test1", password="test123")
+        clan_name = "test-clan"
+        url = reverse("edit_clan_page", kwargs={"clan_name": clan_name})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_edit_clan_page_view_post(self):
+        # edit clan page - POST 
+        data = {
+            "clan_name": "test-clan-update",
+            "content": "ffsdfgsfsdfsdfds-update",
+            "discord_url": "afafagag-update"
+        }
+        clan_name = "test-clan"
+        response = self.client.post(reverse("edit_clan_page", kwargs={"clan_name": clan_name}), data)
+        self.assertEqual(response.status_code, 302)
+
+    def test_delete_clan_view(self):
+        self.client.login(username="test1", password="test123")
+        clan_name = "test-clan"
+        url = reverse("delete_clan", kwargs={"clan_name": clan_name})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
--- index/tests.py
+++ index/tests.py
@@ -1,3 +1,57 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from clan_pages.models import Clan
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your tests here.
+class indexTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        # test user
+        self.user = User.objects.create_user(username="test1", email="test@test.com", password="test123")
+        
+        # dummy clans
+        Clan.objects.create(
+            user=self.user,
+            clan_name="test-clan-1",
+            content="Content for clan 1",
+            discord_url="http://discord1.com"
+        )
+        Clan.objects.create(
+            user=self.user,
+            clan_name="test-clan-2",
+            content="Content for clan 2",
+            discord_url="http://discord2.com"
+        )
+        Clan.objects.create(
+            user=self.user,
+            clan_name="test-clan-3",
+            content="Content for clan 3",
+            discord_url="http://discord3.com"
+        )
+
+    def test_index_page_view(self):
+        """Test the index page view loads correctly with clan pages"""
+        url = reverse("index")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+        
+        # Check the index page to make sure correct clans passed
+        clans = response.context['clans']
+        self.assertEqual(len(clans), 3)
+        self.assertEqual(clans[0].clan_name, "test-clan-1")
+        self.assertEqual(clans[1].clan_name, "test-clan-2")
+        self.assertEqual(clans[2].clan_name, "test-clan-3")
+
+    def test_error_view(self):
+        """Test that the error_view is triggered for a 404 error."""
+        # Make a GET request to a non-existent URL
+        response = self.client.get('incorrect-url/')
+
+        # checks that the page is not found and a 404 error code is returned
+        self.assertEqual(response.status_code, 404)
+
+        # checks that the 404.html is displayed due to incorrect url
+        self.assertTemplateUsed(response, '404.html')
