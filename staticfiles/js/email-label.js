463181b48019d40e15dc52901f0491fc5db87239
Giulio Montaque <giuliom@hotmail.co.uk>
2024-06-18 04:34:48 +0000

    add js change to staticfiles

--- staticfiles/js/email-label.js
+++ staticfiles/js/email-label.js
@@ -1,3 +1,5 @@
 // updates label text of registration form
 const emailLabel = document.querySelector('#id_email').previousElementSibling;
 emailLabel.innerHTML = 'Email:';
+const emailInput = document.querySelector('#id_email')
+emailInput.required = true;
